0488730fa1985440274bb9223ac005cee3d69bfe
literalice <literalice@monochromeroad.com>
2012-12-09 22:48:03 +0900

    updates the release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,8 +56,8 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.0.3",
-              ":rest-client-builder:1.0.2") {
+              ":release:2.2.0",
+              ":rest-client-builder:1.0.3") {
             export = false
         }
         compile(":mongodb:1.0.0.GA") {
